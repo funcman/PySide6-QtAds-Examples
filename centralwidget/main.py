e32c9fd36604ce7988b33d8ea5b6ef85b81d5bd8
funcman <hyq1986@gmail.com>
2024-03-07 17:13:48 +0800

    convert centralwidget code to pyside6

--- centralwidget/main.py
+++ centralwidget/main.py
@@ -1,30 +1,30 @@
 import os
 import sys
 
-from PyQt5 import uic
-from PyQt5.QtCore import Qt, QTimer, QDir, QSignalBlocker
-from PyQt5.QtGui import QCloseEvent, QIcon
-from PyQt5.QtWidgets import (QApplication, QLabel, QCalendarWidget, QFrame, QTreeView,
+from PySide6.QtUiTools import loadUiType
+from PySide6.QtCore import Qt, QTimer, QDir, QSignalBlocker
+from PySide6.QtGui import QCloseEvent, QIcon, QAction
+from PySide6.QtWidgets import (QApplication, QLabel, QCalendarWidget, QFrame, QTreeView,
                              QTableWidget, QFileSystemModel, QPlainTextEdit, QToolBar,
-                             QWidgetAction, QComboBox, QAction, QSizePolicy, QInputDialog)
+                             QWidgetAction, QComboBox, QSizePolicy, QInputDialog)
 
-import PyQtAds as QtAds
+import PySide6QtAds as QtAds
 
 UI_FILE = os.path.join(os.path.dirname(__file__), 'mainwindow.ui')
-MainWindowUI, MainWindowBase = uic.loadUiType(UI_FILE)
-    
+MainWindowUI, MainWindowBase = loadUiType(UI_FILE)
+
 class MainWindow(MainWindowUI, MainWindowBase):
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.setupUi(self)
- 
+
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.OpaqueSplitterResize, True)
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.XmlCompressionEnabled, False)
         QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.FocusHighlighting, True)
         self.dock_manager = QtAds.CDockManager(self)
-        
+
         # Set central widget
         text_edit = QPlainTextEdit()
         text_edit.setPlaceholderText("This is the central editor. Enter your text here.")
@@ -32,7 +32,7 @@ class MainWindow(MainWindowUI, MainWindowBase):
         central_dock_widget.setWidget(text_edit)
         central_dock_area = self.dock_manager.setCentralWidget(central_dock_widget)
         central_dock_area.setAllowedAreas(QtAds.DockWidgetArea.OuterDockAreas)
-        
+
         # create other dock widgets
         table = QTableWidget()
         table.setColumnCount(3)
@@ -44,7 +44,7 @@ class MainWindow(MainWindowUI, MainWindowBase):
         table_dock_widget.setMinimumSize(200, 150)
         table_area = self.dock_manager.addDockWidget(QtAds.DockWidgetArea.LeftDockWidgetArea, table_dock_widget)
         self.menuView.addAction(table_dock_widget.toggleViewAction())
-        
+
         table = QTableWidget()
         table.setColumnCount(5)
         table.setRowCount(1020)
@@ -66,9 +66,9 @@ class MainWindow(MainWindowUI, MainWindowBase):
         properties_dock_widget.setMinimumSize(200, 150)
         self.dock_manager.addDockWidget(QtAds.DockWidgetArea.RightDockWidgetArea, properties_dock_widget, central_dock_area)
         self.menuView.addAction(properties_dock_widget.toggleViewAction())
-        
+
         self.create_perspective_ui()
-        
+
     def create_perspective_ui(self):
         save_perspective_action = QAction("Create Perspective", self)
         save_perspective_action.triggered.connect(self.save_perspective)
@@ -76,23 +76,23 @@ class MainWindow(MainWindowUI, MainWindowBase):
         self.perspective_combobox = QComboBox(self)
         self.perspective_combobox.setSizeAdjustPolicy(QComboBox.AdjustToContents)
         self.perspective_combobox.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.perspective_combobox.activated[str].connect(self.dock_manager.openPerspective)
+        self.perspective_combobox.textActivated.connect(self.dock_manager.openPerspective)
         perspective_list_action.setDefaultWidget(self.perspective_combobox)
         self.toolBar.addSeparator()
         self.toolBar.addAction(perspective_list_action)
         self.toolBar.addAction(save_perspective_action)
-        
+
     def save_perspective(self):
         perspective_name, ok = QInputDialog.getText(self, "Save Perspective", "Enter Unique name:")
         if not ok or not perspective_name:
             return
-        
+
         self.dock_manager.addPerspective(perspective_name)
         blocker = QSignalBlocker(self.perspective_combobox)
         self.perspective_combobox.clear()
         self.perspective_combobox.addItems(self.dock_manager.perspectiveNames())
         self.perspective_combobox.setCurrentText(perspective_name)
-        
+
     def closeEvent(self, event: QCloseEvent):
         self.dock_manager.deleteLater()
         super().closeEvent(event)
@@ -100,7 +100,7 @@ class MainWindow(MainWindowUI, MainWindowBase):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    
+
     w = MainWindow()
     w.show()
-    app.exec_()
+    app.exec()
